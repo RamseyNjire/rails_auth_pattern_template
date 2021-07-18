447c49b43dc0617336bef9f8b367cb28425cf2ab
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 13:52:42 +0300

    write validations for user model and password getter and setter methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    attr_reader :password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
 end
