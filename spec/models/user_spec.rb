3169dca451c933f43007d79a83a9e5e41936b775
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 14:11:51 +0300

    correct directive in .rspec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user){ build(:user) }
+
+  describe "validations" do
+    it { should validate_presence_of(:username)}
+  end
 end
