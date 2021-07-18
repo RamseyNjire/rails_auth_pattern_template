1f36e93d3c423b14f88f363a92a9f36aae6a35b9
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 16:34:58 +0300

    write code to pass test for create method in users controller with valid params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_url(@user)
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
