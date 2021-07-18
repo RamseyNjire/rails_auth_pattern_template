a3b3a3e106c8653a4dd518107c5d2ce96da23ea4
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 13:40:53 +0300

    create database and run migration for creating users table

--- db/migrate/20210718103819_create_users.rb
+++ db/migrate/20210718103819_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
 
+      t.index :username, unique: true
       t.timestamps
     end
   end
