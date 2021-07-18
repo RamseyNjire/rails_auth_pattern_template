67dacc5091cfe856f26aa475cf810a4ae741130c
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 17:39:46 +0300

    build and test create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,18 @@ class SessionsController < ApplicationController
     def new
         render :new
     end
+
+    def create
+        user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if user.nil?
+            flash[:errors] = "The username or password is incorrect"
+            render :new
+        else
+            redirect_to user_url(user)
+        end
+    end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,10 +2,37 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
     subject(:user){ build(:user) }
+    
     describe "GET #new" do
         it "renders the new template" do
             get :new, params: {}
             expect(response).to render_template(:new)
+        end
+    end
+
+    describe "POST #create" do
+        before { user.save! }
+        context "with valid params" do
+            it "redirects to the user show page" do
+                post :create, params: { user: {
+                                                username: "Caligula",
+                                                password: "Password"
+                } }
+
+                expect(response).to redirect_to(user_url(user))
+            end
+        end
+
+        context "with invalid params" do
+            it "validates the params" do
+                post :create, params: { user: {
+                                                username: "NotCaligula",
+                                                password: "NotPassword"
+                } }
+
+                expect(response).to render_template(:new)
+                expect(flash[:errors]).to be_present
+            end
         end
     end
 end
