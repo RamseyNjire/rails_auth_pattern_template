941708a3324ed8a1a08fda8ca22ccf375901b74f
Ramsey Njire <ramseynjire@gmail.com>
2021-07-18 17:22:26 +0300

    write code to pass tests for find_by_credentials method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,11 @@ class User < ApplicationRecord
     def is_password?(password)
         BCrypt::Password.new(self.password_digest).is_password?(password)
     end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        
+        user.is_password?(password) ? user : nil
+    end
 end
