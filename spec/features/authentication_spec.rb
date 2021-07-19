b47afb29f5b074e636d65f7df3c86851d97cc849
Ramsey Njire <ramseynjire@gmail.com>
2021-07-19 16:42:32 +0300

    write integration tests to ensure login process is working properly

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -26,6 +26,22 @@ feature "the signup process" do
     end
 
     feature "signing in a user" do
+        subject(:user) { create(:user) }
+        before do
+            visit new_session_url
+            fill_in "Username:", with: user.username
+            fill_in "Password:", with: user.password
+
+            click_on "Login"
+        end
+
+        scenario "redirects to user show page after signup" do
+            expect(page).to have_content "Caligula"
+        end
+
+        scenario "logout button is visible" do
+            expect(page).to have_button("Logout")
+        end
     end
 
 end
