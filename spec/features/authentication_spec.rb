41ea8a107440dea1923ed7659301f52be3181f86
Ramsey Njire <ramseynjire@gmail.com>
2021-07-19 16:38:07 +0300

    write integration tests to ensure signup process is working properly

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature "the signup process" do
+
+    scenario 'has a new user page' do
+        visit new_user_url
+        expect(page).to have_content 'Create Account'
+    end
+
+    feature "signing up a user" do
+        before do
+            visit new_user_url
+            fill_in "Username:", with: "Augustus"
+            fill_in "Password:", with: "Password"
+
+            click_on "Create Account"
+        end
+
+        scenario "redirects to user show page after signup" do
+            expect(page).to have_content "Augustus"
+        end
+
+        scenario "logout button is visible" do
+            expect(page).to have_button("Logout")
+        end
+    end
+
+    feature "signing in a user" do
+    end
+
+end
